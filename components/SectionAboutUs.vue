a8e2c3ccf6b2e3488a50621eae087b0596be34e3
Muhammad Oktoluqman Fakhrianto <dimas.oktoluqman@gmail.com>
2024-12-05 11:45:54 +0700

    feat: responsive SectionAboutUs

--- components/SectionAboutUs.vue
+++ components/SectionAboutUs.vue
@@ -3,35 +3,39 @@ import { sectionAboutUs as content } from '~/content'
 </script>
 <template>
   <div class="bg-primary-container">
-    <VContainer class="pb-6 d-flex justify-space-between">
+    <VContainer class="pb-6 overflow-hidden position-relative">
       <div style="max-width: 400px">
-        <p class="text-h5 text-tertiary font-weight-bold">
+        <p class="text-h6 text-sm-h5 text-tertiary font-weight-bold">
           {{ content.title }}
         </p>
-        <p class="text-h4 font-weight-bold text-primary mb-3 mt-2">
+        <p class="text-h5 text-sm-h4 font-weight-bold text-primary mb-3">
           {{ content.header }}
         </p>
-        <p class="text-h6">{{ content.text }}</p>
-        <div>
+        <p>{{ content.text }}</p>
+        <div class="my-2">
           <div
             v-for="(i, idx) in content.items"
             :key="idx"
             class="d-flex align-center"
           >
             <VIcon icon="mdi-check-circle" color="tertiary" />
-            <p class="text-h6 ms-2">{{ i }}</p>
+            <p class="ms-2">{{ i }}</p>
           </div>
         </div>
         <VBtn append-icon="mdi-chevron-right" class="mt-3">
           Pelajari Lebih Lanjut
         </VBtn>
       </div>
-      <NuxtImg
-        src="/images/car-unit/jeep-rubicon.png"
-        quality="40"
-        width="600"
-        class="mb-n6"
-      />
+      <div
+        class="position-absolute bottom-0 right-0 d-none d-sm-block mb-6 mb-md-0"
+      >
+        <NuxtImg
+          src="/images/car-unit/jeep-rubicon.png"
+          quality="40"
+          sizes="60vw md:600"
+          class="mb-n6"
+        />
+      </div>
     </VContainer>
   </div>
 </template>
