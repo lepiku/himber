18f01219f49ff8b901d5f223b7f5340a18b50930
Muhammad Oktoluqman Fakhrianto <dimas.oktoluqman@gmail.com>
2024-12-05 10:38:07 +0700

    feat: responsive SectionCover

--- components/SectionCover.vue
+++ components/SectionCover.vue
@@ -10,12 +10,13 @@ const darkBg = appTheme.palettes.primary[5]
 </script>
 
 <template>
-  <div class="bg-primary">
-    <div class="landing-cover" :style="backgroundStyles">
-      <VContainer class="py-8">
+  <div class="bg-primary d-flex">
+    <div class="landing-cover-start"></div>
+    <div class="landing-cover flex-fill" :style="backgroundStyles">
+      <VContainer class="py-8 ms-0">
         <div class="text-wrapper">
-          <p class="text-h3">Sewa Doang?</p>
-          <p class="text-h1">Gampang!</p>
+          <p class="text-h4 text-sm-h3">Sewa Doang?</p>
+          <p class="text-h2 text-sm-h1">Gampang!</p>
           <p class="my-6">
             Sewa mobil kami dengan harga terjangkau, armada lengkap, dan layanan
             profesional. Cocok untuk perjalanan bisnis, liburan, atau kebutuhan
@@ -34,7 +35,11 @@ const darkBg = appTheme.palettes.primary[5]
   </div>
 </template>
 
-<style lang="sass">
+<style lang="sass" scoped>
+.landing-cover-start
+  background: v-bind(darkBg)
+  width: calc((100vw - 1200px)/2)
+
 .landing-cover
   background-size: cover
   background-position: center
@@ -43,5 +48,5 @@ const darkBg = appTheme.palettes.primary[5]
     background-image: linear-gradient(to right, v-bind(darkBg), transparent)
 
   .v-container .text-wrapper
-    max-width: 400px
+    max-width: 450px
 </style>
